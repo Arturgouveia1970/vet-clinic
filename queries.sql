d838daac8db59322c443759a3e5c385616fa39e6
Arturgouveia1970 <artgouveiag@gmail.com>
2022-07-18 12:23:32 +0100

    added animals table and queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals where name like '%mon';
+select name from animals where date_of_birth BETWEEN '2016-01-01' and '2019-01-01';
+select name from animals where neuterd = true and escape_attemps < 3;
+select date_of_birth from animals where name in ('Agumon', 'Pikachu');
+select name, escape_attemps from animals where weight_kg > 10.5;
+select * from animals where neuterd = true;
+select * from animals where name NOT in ('Gabumon');
+select * from animals WHERE weight_kg >=10.4 and weight_kg <=17.3;
