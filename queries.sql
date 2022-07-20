bf47e9db30a327620f77e8d4d8e75f8b96106d4a
Arturgouveia1970 <artgouveiag@gmail.com>
2022-07-20 16:43:15 +0100

    made queries to multiple tables

--- queries.sql
+++ queries.sql
@@ -17,4 +17,31 @@ select count(escape_attemps) from animals where neuterd = true;
 select count(escape_attemps) from animals where neuterd = false;
 select max(weight_kg) from animals group by species;
 select min(weight_kg) from animals group by species;
-select avg(escape_attemps),species from animals where date_of_birth between '1990-01-01' and '2000-12-31' group by species;
+select avg(escape_attemps),species from animals
+  where date_of_birth between '1990-01-01' and '2000-12-31'
+  group by species;
+
+--join queries
+select name, full_name from animals 
+  join owners on animals.owner_id = owners.id 
+  where full_name = 'Melody Pond';
+select animals.name as ANIMALS, species.name as SPECIES from animals
+  join species on animals.species_id = species.id 
+  where species.name = 'Pokemon';
+select name as ANIMALS, full_name as OWNERS from animals 
+  right join owners on animals.owner_id = owners.id;
+select count(animals.name) as number_of_animals, species.name as specie from animals
+  join species on animals.species_id = species.id 
+  group by species.name;
+select full_name as owner, animals.name as animals_name, species.name as name_of_species from animals 
+  join owners on owners.id = animals.owner_id 
+  join species on species.id = animals.species_id 
+  where owners.full_name = 'Jennifer Orwell' and species.name = 'Digimon';
+  select name, escape_attemps,full_name from animals
+    join owners
+    on owners.id = animals.owner_id 
+    where owners.full_name = 'Dean Winchester' and animals.escape_attemps = 0;
+select full_name, count(animals.owner_id) as Owns from owners
+ join animals on animals.owner_id = owners.id 
+ group by full_name 
+ order by Owns desc;
