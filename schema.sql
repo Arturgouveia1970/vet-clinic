6683f2dce7b544ab80aa6100a033d4f94228a408
Arturgouveia1970 <artgouveiag@gmail.com>
2022-07-21 10:12:42 +0100

    added vets table and the 2 join tables

--- schema.sql
+++ schema.sql
@@ -30,8 +30,32 @@ ALTER TABLE animals
   FOREIGN KEY (species_id) 
   REFERENCES species(id);
 
- ALTER TABLE animals ADD COLUMN owner_id INTEGER;
- ALTER TABLE animals 
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ALTER TABLE animals 
    ADD CONSTRAINT fk_owners 
    FOREIGN KEY(owner_id)
    REFERENCES owners(id);
+
+CREATE TABLE vets (
+   id serial PRIMARY KEY NOT NULL,
+   name VARCHAR(50) NOT NULL,
+   age INTEGER,
+   date_of_graduation DATE
+);
+
+CREATE TABLE specialization (
+  id SERIAL PRIMARY KEY NOT NULL,
+  species_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vet_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY NOT NULL,
+  animal_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  date_of_visit DATE NOT NULL,
+  CONSTRAINT fk_visit_animal FOREIGN KEY(animal_id) REFERENCES animals(id),
+  CONSTRAINT fk_visit_vet FOREIGN KEY(vet_id) REFERENCES vets(id)
+  );
